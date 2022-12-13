7111ebfc6df62bed475873597a57fbc691a15680
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-12-13 14:01:44 -0300

    Native Host e User
    Pesquisa de métodos e objetos nativos

---
+++ 03-Objetos/03-Native-Host-e-User/script.js
@@ -0,0 +1,30 @@
+/* Liste 5 Objetos Nativos */
+// Object
+// String
+// Array
+// function
+// Map
+
+// Saiba Mais: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects
+
+/* Liste 5 objetos do Browser */
+// Window
+// focus
+// navigator
+// navigation
+// statusbar
+
+/* Liste 2 métodos, Propriedade ou Objetos
+  Presentes no chrome que não existem no firefox
+*/
+
+if (typeof document.contains !== "undefined") {
+  console.log("Existe");
+} else {
+  console.log("não existe");
+}
+
+/* 
+Chromes: addEventListener, contains
+
+*/
