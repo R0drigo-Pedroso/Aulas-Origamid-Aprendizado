c9f57de6819d47df81e6aa0a722334816f54ddae
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-12-14 12:44:59 -0300

    String, aula incompleta

---
+++ 03-Objetos/04-String/script.js
@@ -0,0 +1,31 @@
+// const comida = "Pizza";
+// const agua = new String("Agua");
+
+// // console.log(agua.length);
+
+// const frase = "a melhor comida";
+// console.log(frase[frase.length - 1]);
+// console.log(frase.charAt(5));
+
+const frase = "A melhor linguagem é ";
+const linguagem = "JavaScript";
+
+const fraseFinal = frase.concat(linguagem, "!!!");
+
+console.log("--------------------------------------");
+
+const fruta = "Banana";
+const listaFrutas = "Melão, Laranja, Pera";
+
+console.log(fruta.includes(listaFrutas));
+
+console.log(fruta.endsWith("na"));
+
+console.log("--------------------------------------");
+
+const transacao1 = "A melhor lorem da vida ";
+const transacao2 = "A melhor linguagem é linguagem é ";
+const transacao3 = "A melhor linguagem é linguagem é linguagem é ";
+
+console.log(transacao1.slice(25));
+/* Posso ultilizar numero nagativo */
